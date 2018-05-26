f8c6ea741df248ef9c3dd699295f6cbec501b52a
Thanh Huyen <teresathanhhuyen1804@gmail.com>
2018-05-26 17:55:41 +0700

    zczxczxc
    zczxcx

---
+++ Ánh-Hiếu-Huyền_QuanLyNhaSach_S3/DTO_QLNS/ET_TacGia.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DTO_QLNS
+{
+    public class ET_TacGia
+    {
+        private string _maTG;
+
+        public string MaTG
+        {
+            get { return _maTG; }
+            set { _maTG = value; }
+        }
+
+        private string _tenTG;
+
+        public string TenTG
+        {
+            get { return _tenTG; }
+            set { _tenTG = value; }
+        }
+
+        private int _namSinh;
+
+        public int NamSinh
+        {
+            get { return _namSinh; }
+            set { _namSinh = value; }
+        }
+
+        public ET_TacGia(string maTG, string tenTG, int namSinh)
+        {
+            this._maTG = maTG;
+            this._tenTG = tenTG;
+            this._namSinh = namSinh;
+        }
+
+    }
+}
