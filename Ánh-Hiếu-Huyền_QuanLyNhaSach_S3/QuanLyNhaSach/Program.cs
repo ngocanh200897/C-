099ba292ca1543e0f09be2110ce8a8a122aa963a
Thanh Huyen <teresathanhhuyen1804@gmail.com>
2018-05-26 18:03:02 +0700

    huyen mach
    dcvxdvxcb

--- Ánh-Hiếu-Huyền_QuanLyNhaSach_S3/QuanLyNhaSach/Program.cs
+++ Ánh-Hiếu-Huyền_QuanLyNhaSach_S3/QuanLyNhaSach/Program.cs
@@ -15,7 +15,7 @@ namespace QuanLyNhaSach
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new frmNhanVien());
+            Application.Run(new FrmKho());
         }
     }
 }
